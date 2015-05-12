6b93915933c251e925e9ed18cf02e9073f533060
cachrisman <charlie.chrisman@gmail.com>
2015-05-11 18:05:09 -0700

    added function render to add images from JSON data to body, in document on ready, get JSON data of trending top 25 from giphy and render it, then add submit event listener for when user searches and get JSON search result and render.  had to wrap input element in a form element for submit event listener to work

--- base.js
+++ base.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){
-  // code in here!
-  console.log("here's your data:", mockData)
-})
+var render = function(responseData) {
+    var data = responseData.data;
+    data.forEach(function(item, i) {
+        $("body").append("<img src='" + item.images.fixed_height.url + "'>");
+    });
+};
+
+$(document).on("ready", function() {
+    $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC", render);
+    $('form').on("submit", function(e){
+        e.preventDefault();
+        var query = $('input').val().toLowerCase().replace(/\s+/g, "-");
+        $('img').remove();
+        $.get("http://api.giphy.com/v1/gifs/search?q="+query+"&api_key=dc6zaTOxFJmzC", render);
+    });
+});
