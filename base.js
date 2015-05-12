4b21bdda8b5050854df7abba400022f8d0442971
cachrisman <charlie.chrisman@gmail.com>
2015-05-11 19:28:41 -0700

    fixed infinite scroll logic to get next 25 new gifs.

--- base.js
+++ base.js
@@ -7,6 +7,7 @@ var render = function(responseData) {
 
 $(document).on("ready", function() {
     var query = null;
+    var limit = 25;
     $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC", render);
     $('form').on("submit", function(e) {
         e.preventDefault();
@@ -16,18 +17,10 @@ $(document).on("ready", function() {
     });
 
     $(window).scroll(function() {
-        var limit = 25;
         if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-            // alert("reached bottom!");
-            if (query) {
-                console.log("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=" + limit);
-                $.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=" + limit, render);
-
-            } else {
-                console.log("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit);
-                $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit, render);
-            }
+            if (query) $.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=" + limit, render);
+            else $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit, render);
+            limit += 25;
         }
-        // limit += 25;
     });
 });
